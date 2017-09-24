4f6258c6209635d7eb44dc99a5a00bdcbbb01b1e
Rafael Toledo <rafaeltol@gmail.com>
2017-09-24 13:11:16 -0300

    general improvements in code to train lstm model

--- gerando-letras-1.R
+++ gerando-letras-1.R
@@ -11,7 +11,7 @@ library(tokenizers)
 link_base <- 'https://www.letras.mus.br'
 
 # listando os links
-ws_links <- paste0(link_base, '/wesley-safadao/') %>% 
+ws_links <- paste0(link_base, '/legiao-urbana/') %>% 
         rvest::html_session() %>% 
         rvest::html_nodes('.cnt-list--alp > ul > li > a') %>% 
         rvest::html_attr('href')
@@ -68,11 +68,11 @@ ws_letras <- unlist(purrr::map(ws_links, ~{
 }))
 
 cat(head(ws_letras, which(ws_letras == 'end_lyrics')[1] - 1), sep = ' ')
-
-
+saveRDS(ws_letras, "ws_letras.rds")
+ws_letras <- readRDS("./ws_letras.rds")
 
 #--------------------------------------------------------------------------------------
-# Filtra dados, seleciona somente pavras mais relevantes
+# Filtra dados, seleciona somente palavras mais relevantes
 # Estrutura dados com relação temporal
 #--------------------------------------------------------------------------------------
 
@@ -82,10 +82,10 @@ data <- tibble(X = ws_letras) %>%
 words_selected <- data %>% 
                         group_by(X) %>% 
                         summarise(count=n()) %>% 
-                        filter(count>50) %>% 
+                        filter(count>10) %>% 
                         select(X) %>% 
                         unlist()
-
+saveRDS(words_selected, "helpers/words-lexical.rds")
 
 # preparing the words sequence
 data_selected <- data %>% 
@@ -103,9 +103,44 @@ data_selected <- data %>%
 end_paragraph_idx <- data_selected$Y %>%  unlist() %>% {.=="end_paragraph"} %>% which()
 end_paragraph_idx_not <- data_selected$Y %>%  unlist() %>% {.!="end_paragraph"} %>% which()
 words_idx_selected <- c(end_paragraph_idx_not,
-                        sample(end_paragraph_idx, 1500)
+                        sample(end_paragraph_idx, 50)
                         )
-data_selected <- data_selected %>% slice(words_idx_selected)
+
+
+data_selected_sub <- data_selected %>% slice(words_idx_selected)
+
+# exclude `Y` with start_lyrics
+data_selected_sub %>% 
+        filter(Y!="start_lyrics") %>% 
+        {.} -> data_selected_sub
+
+
+# exclude lines with more than 2 `end_paragraph` side-by-side
+condition_idx <- c()
+data_selected_sub %>% 
+        dim(.) %>% 
+        `[`(2) %>% 
+        seq(.) %>% 
+        head(., -2) %>% 
+        for( i in .){
+                data_selected_sub %>% 
+                        select(i, i+1, i+2) %>% 
+                        `==`(., "end_paragraph") %>% 
+                        rowSums() %>% 
+                        `>=`(., 2) %>% 
+                        which() %>% 
+                        { condition_idx <<- c(condition_idx, .) }
+        }
+condition_idx <- unique(condition_idx)
+
+data_selected_sub <- data_selected_sub %>% 
+                        filter(!row_number() %in% condition_idx)
+
+# do a generic sample to contorl Array sizes
+data_selected_sub %>% 
+        sample_n(30e3) %>%
+        {.} -> data_selected_sub
+        
 
 
 # features_dim
@@ -120,10 +155,11 @@ for( w in words_selected){
         words_encoding[[w]] <- aux
 }
 
-#data_selected_X <- data_selected %>% select(X5, X4, X3, X2, X1, X)
-timesteps <- 4
-data_selected_X <- data_selected %>% select(paste0("X", timesteps:1))
-data_selected_Y <- data_selected %>% select(Y)
+saveRDS(words_encoding, "./helpers/words-enconding.rds")
+
+timesteps <- 3
+data_selected_X <- data_selected_sub %>% select(paste0("X", timesteps:1))
+data_selected_Y <- data_selected_sub %>% select(Y)
 
 
 #--------------------------------------------------------------------------------------
@@ -147,7 +183,8 @@ capture_encode <- function(rowX, colX){
 get_sample <- function(rowX, colX=1:timesteps){
         X_train <- lapply(X=colX, capture_encode, rowX=rowX) %>% 
                         lapply(as.numeric) %>% 
-                        as.data.frame() %>% unname() %>% 
+                        as.data.frame() %>% 
+                        unname() %>% 
                         as.matrix()        
         
         X_train <- t(X_train)
@@ -162,14 +199,17 @@ get_sample <- function(rowX, colX=1:timesteps){
 
 
 
+
+
 #--------------------------------------------------------------------------------------
 # Transform the whole dataset in one-hot-enconding format
 #--------------------------------------------------------------------------------------
-
-train_onehot_X <- array(data=NA, dim=c(nrow(data_selected), timesteps, words_n))
-train_onehot_Y <- array(data=NA, dim=c(nrow(data_selected), words_n))
+gc()
+train_onehot_X <- array(data=NA, dim=c(nrow(data_selected_sub), timesteps, words_n))
+train_onehot_Y <- array(data=NA, dim=c(nrow(data_selected_sub), words_n))
 for( i in seq(nrow(data_selected_X))){
         s <- get_sample(i)
+        
         train_onehot_X[i, , ] <- s$X_train
         train_onehot_Y[i, ] <- s$Y_train        
 }
@@ -179,39 +219,48 @@ for( i in seq(nrow(data_selected_X))){
 # Training      
 #--------------------------------------------------------------------------------------
 
-units_n <- 32*4
-batch_size <- 66504
-model <- keras_model_sequential() %>% 
-                layer_lstm(units_n, batch_input_shape=c(batch_size, timesteps, words_n)) %>% 
-                layer_dense(words_n) %>% 
-                layer_activation("softmax")
-
-model %>% 
-        compile( loss = "categorical_crossentropy",
-                 optimizer = optimizer_adam(lr=1e-4) )
-
-# validation_split=0.112478
+units_n <- 4
+batch_size <- 16
 idx_until <- (nrow(train_onehot_X) %/% batch_size) * batch_size
-model %>%    
-        fit(x=train_onehot_X[1:idx_until,,], y=train_onehot_Y[1:idx_until,], 
-                epochs=50, stateful=TRUE, shuffle=TRUE, 
-                batch_size=batch_size, validation_split=0) %>% 
+# the `5`guarantee the split of the validation set
+idx_until <- (nrow(train_onehot_X)%/%(5*32))*(5*32)
+
+rm(model)
+rm(model_new)
+gc() 
+model <- keras_model_sequential() %>% 
+                layer_lstm(units_n, batch_input_shape=c(batch_size, timesteps, words_n)) %>%
+                layer_dense(words_n, activation="softmax") %>% 
+                compile( loss = "categorical_crossentropy", optimizer = optimizer_adam(lr=1e-4) ) 
+
+
+model %>% fit(x=train_onehot_X[1:idx_until,,], y=train_onehot_Y[1:idx_until,], 
+                epochs=5, stateful=FALSE, shuffle=TRUE, 
+                batch_size=batch_size, validation_split=0.20) %>% 
                 {.} -> model_loss_hist
 
+# 4.2291
+# 4.2104 
 plot(model_loss_hist$metrics$loss, col="red", type="l", ylim=c(0,6))
 lines(model_loss_hist$metrics$val_loss, col="green", type="l")
 
 model_new <- keras_model_sequential() %>% 
-                layer_lstm(units_n, batch_input_shape=c(1, timesteps, words_n)) %>% 
-                layer_dense(words_n) %>% 
-                layer_activation("softmax")
+                layer_lstm(units_n, batch_input_shape=c(1, timesteps, words_n)) %>%
+                layer_dense(words_n, activation="softmax")
+
+
+
+
+
 
 model_new$set_weights(get_weights(model)) 
 
 
-save_model_hdf5(model_new, "model-250-epochs.rds")
+save_model_hdf5(model_new, "./models/last-release.h5")
+model_new <- load_model_hdf5("./models/last-release.h5")
 
-X_subset_idx <- sample(nrow(data_selected_X), 1e4)
+
+X_subset_idx <- sample(nrow(data_selected_X), 1e2)
 X_subset_idx <- seq(nrow(data_selected_X))
 X_subset <- data_selected_X[X_subset_idx, ]
 X_subset$Y <- data_selected_Y$Y[X_subset_idx]
@@ -223,7 +272,7 @@ for( i in seq(nrow(X_subset))){
         X_subset$H[i] <- words_selected[preds_idx]
 }
 X_subset$H %>% table() %>% sort(T)
-View(X_subset)
+#View(X_subset)
 
 
 #--------------------------------------------------------------------------------------
@@ -246,12 +295,17 @@ generate_lyrics <- function(n_diversity=5, n_diversity_min=3){
         for( i in seq(100)){
                 
                 n_diversity_adj <- (n_diversity - n_diversity_min)*exp(-2*i/10) + n_diversity_min
+
+                x_sample %>% 
+                        sapply(., one_hot_encoding) %>% 
+                        t() %>% 
+                        array(data=., dim=c(1, dim(.))) %>% 
+                        {.} -> x_sample_encoded
                 
-                x_sample_encoded <- sapply(x_sample, one_hot_encoding)
-                y_sample_preds <- predict(model_new, test_sample)
-                
+                y_sample_preds <- predict(model_new, x_sample_encoded)
                 y_samples_best_preds <- get_best_indices(y_sample_preds, n=n_diversity_adj)
-                # just to adjust function sample, which does not recognize a vector of one dim
+
+                # just to adjust function sample, which does not recognize a vector of length one
                 if( length(y_samples_best_preds)==1 ){
                         y_samples_best_preds <- rep(y_samples_best_preds, 2)
                 }
@@ -261,9 +315,8 @@ generate_lyrics <- function(n_diversity=5, n_diversity_min=3){
                 
                 lyrics <- c(lyrics, y_sample_word)
                 x_sample <- c(x_sample[-1], y_sample_word)
-                
                 if( y_sample_word == "end_lyrics"){
-                        return(lyrics)
+                        return(lyrics[-length(lyrics)])
                 }
                 
         }
@@ -271,16 +324,17 @@ generate_lyrics <- function(n_diversity=5, n_diversity_min=3){
         return(lyrics)
 }
 
+
 print_lyrics <- function(xlyrics){
         xlyrics[ ! xlyrics %in% c("start_lyrics")] %>% 
                 paste0(collapse=" ") %>% 
                 strsplit("end_paragraph") %>% 
                 unlist() %>% 
-                return()
+                print()
 }
 
 
-generate_lyrics(100, 10) %>% 
+generate_lyrics(100, 20) %>% 
         print_lyrics()
 
 
